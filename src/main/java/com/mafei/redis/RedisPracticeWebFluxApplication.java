86d98abe3482be8e615b4a348c633d79296361ff
mafei <mafei.dev@gmail.com>
2021-09-15 19:04:47 +0530

    Initial commit

---
+++ src/main/java/com/mafei/redis/RedisPracticeWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.mafei.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPracticeWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPracticeWebFluxApplication.class, args);
+    }
+
+}
